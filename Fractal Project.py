c95c964f406942b3a42d3c0687dc4361f8c8b732
awesome1238765 <60079834+awesome1238765@users.noreply.github.com>
2024-05-01 14:04:46 -0400

    Update Fractal Project.py
    WHOOOOOOOOOOO DAY 1

--- Fractal Project.py
+++ Fractal Project.py
@@ -1,15 +1,23 @@
 import turtle
+import math
 drawTurtle = turtle.Turtle()
 
-def pythagorusFractalTree(size, turtleType, iterations):
-    makeSquare(size, turtleType)
+def pythagorusFractalTree(size, turtleType, iterations,):
+    turtleType.setheading(90)
+    if iterations == 0:
+        for x in range(0, 4):
+            turtleType.forward(size)
+            turtleType.right(90)
+    else:
+        for x in range(0 , 4):
+            turtleType.forward(size)
+            turtleType.left(30)
+            pythagorusFractalTree(size * ((2 ** 1/2) / 2), turtleType, (iterations - 1))
+            turtleType.left(30)
 
-def makeSquare(length, turtleType):
-    for x in range(0, 4):
-        turtleType.forward(length)
-        turtleType.right(90)
 
-pythagorusFractalTree(100, drawTurtle, 5)
+
+pythagorusFractalTree(100, drawTurtle, 1)
 
 #More code do disregard to make sure VS doesn't instantaneously explode upon running it.
 input()
